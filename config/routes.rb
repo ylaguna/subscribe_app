a2f29c8f1a54fc48f62fc8a37985cf94081f15b9
Yan Laguna <yan.laguna@gmail.com>
2017-08-27 15:16:11 -0300

    working authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :admins
+  devise_for :admin
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
